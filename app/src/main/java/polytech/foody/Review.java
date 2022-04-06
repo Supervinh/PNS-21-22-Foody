5ba47f3112cd13695486c0fb69cb82bc5cc0bf97
Vinh <narutovinh@hotmail.fr>
2022-04-06 16:58:28 +0200

    Modification review

--- app/src/main/java/polytech/foody/Review.java
+++ app/src/main/java/polytech/foody/Review.java
@@ -4,7 +4,7 @@ public class Review extends Post{
     int score;
 
     public Review(String content, Post commentedPost, User author, Restaurant place, String image, int score) {
-        super(content, commentedPost, author, place, image);
+        super(content, author, place, image);
         this.score = score;
     }
 }
