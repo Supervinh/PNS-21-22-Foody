88aa58832e1834ef47f404b63c119a130b4bd8b6
Vinh <narutovinh@hotmail.fr>
2022-04-06 15:11:58 +0200

    Reservation

--- app/src/main/java/polytech/foody/Reservation.java
+++ app/src/main/java/polytech/foody/Reservation.java
@@ -1,4 +1,12 @@
 package polytech.foody;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Reservation {
+    String nomReservateur;
+    int nombrePersonnes;
+    Date dateReservation;
+    Time heureReservation;
+    String commentaires;
 }
