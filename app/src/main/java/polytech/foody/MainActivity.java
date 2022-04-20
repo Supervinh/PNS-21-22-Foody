595c537c16cded201801435543ab1e164fde60c8
Vinh <narutovinh@hotmail.fr>
2022-04-20 16:07:44 +0200

    début gestion caméra

--- app/src/main/java/polytech/foody/MainActivity.java
+++ app/src/main/java/polytech/foody/MainActivity.java
@@ -3,10 +3,13 @@ package polytech.foody;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IPictureActivity{
+    private Bitmap picture;
+    private PictureFragment pictureFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/polytech/foody/PictureFragment.java
@@ -0,0 +1,55 @@
+package polytech.foody;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+public class PictureFragment extends Fragment {
+    private ImageView imageView;
+
+    public PictureFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_picture, container, false);
+
+        imageView = rootView.findViewById(R.id.imageView);
+
+        rootView.findViewById(R.id.buttonImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.CAMERA},
+                            IPictureActivity.REQUEST_CAMERA);
+                } else {
+                    takePicture();
+                }
+
+            }
+        });
+
+        return rootView;
+    }
+
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        getActivity().startActivityForResult(intent, IPictureActivity.REQUEST_CAMERA);
+    }
+
+    public void setImage(Bitmap bitmap){
+        imageView.setImageBitmap(bitmap);
+    }
+}
