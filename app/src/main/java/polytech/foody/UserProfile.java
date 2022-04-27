471a91fc6f3c3f916a94f23989814ea0b1689dcc
Vinh <narutovinh@hotmail.fr>
2022-04-27 14:42:01 +0200

    Page myprofiil

--- app/src/main/java/polytech/foody/UserProfile.java
+++ app/src/main/java/polytech/foody/UserProfile.java
@@ -11,7 +11,7 @@ public class UserProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_profile);
+        setContentView(R.layout.my_profile);
 
 
 
